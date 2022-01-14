cadc5d8d618d6226ebcdc054788fdb099efdb156
Mehdi Safari <sa.mehdisafari@gmail.com>
2022-01-14 08:28:08 -0800

    add dropdown

--- client/main.js
+++ client/main.js
@@ -1,3 +1,7 @@
+const selectForm = document.getElementById("select-form")
+
+
+
 document.getElementById("complimentButton").onclick = function () {
     axios.get("http://localhost:4000/api/compliment/")
         .then(function (response) {
