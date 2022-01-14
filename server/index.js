f52257f4d1b244933b96d0709f2d15adda238622
Mehdi Safari <sa.mehdisafari@gmail.com>
2022-01-14 08:14:38 -0800

    add controller.js, edit server/index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const controller = require("./controller")
 
 const app = express();
 
@@ -8,18 +9,12 @@ app.use(cors());
 
 app.use(express.json()); // When we want to be able to accept JSON.
 
-app.get("/api/compliment", (req, res) => {
-  const compliments = ["Gee, you're a smart cookie!",
-					 "Cool shirt!",
-					 "Your Javascript skills are stellar.",
-  ];
+app.get("/api/compliment", controller.getCompliment);
 
-  // choose random compliment
-  let randomIndex = Math.floor(Math.random() * compliments.length);
-  let randomCompliment = compliments[randomIndex];
+app.get("/api/fortune", controller.getFortune);
 
-  res.status(200).send(randomCompliment);
-  
-});
+app.post("/api/display", controller.displayText)
+
+app.delete("/api/display/:id", controller.deleteTask)
 
 app.listen(4000, () => console.log("Server running on 4000"));
